eb1717962523a2948085bdb59a89da7116527149
Gurmeet22 <gsinghs1998@gmail.com>
2019-05-22 09:37:36 +0530

    just for fun

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const url = 'https://api.darksky.net/forecast/897293aac92a9852735dc216648ca991/37.8267,-122.4233';
 
-request({url : url}, (error, response) => {
+request(url, (error, response) => {
     const data = JSON.parse(response.body);
     console.log(data.currently);
 });
