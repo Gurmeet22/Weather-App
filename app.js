21a12238c8324537fc3d2709695ed948797e5b5c
Gurmeet22 <gsinghs1998@gmail.com>
2019-05-22 10:38:58 +0530

    Commited

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request');
-const url = 'https://api.darksky.net/forecast/897293aac92a9852735dc216648ca991/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/897293aac92a9852735dc216648ca991/37.8267,-122.4233?lang=es&units=si';
 
-request(url, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data);
+request({url: url, json: true}, (error, response) => {
+    const data = response.body;
+    console.log(data.daily.data[0].summary+' Currently the temperature is '+data.currently.temperature+' and probability of rain is '+data.currently.precipProbability);
 });
