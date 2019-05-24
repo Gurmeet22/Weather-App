43bc4fdf8405e78f40c1550bdabbfaf0f52aa4a4
Gurmeet22 <gsinghs1998@gmail.com>
2019-05-24 14:49:03 +0530

    Minor change

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About Me',
+        title: 'About this Application',
         name: 'Gurmeet Singh'
     })
 })
