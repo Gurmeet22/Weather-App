df221d9be26444117b2cb25edd578375ad2b7984
Gurmeet22 <gsinghs1998@gmail.com>
2019-05-24 09:48:17 +0530

    Client Side Js added

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('/weather?address=Jamshedpur').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    }).catch(error => {
+        console.log(error);
+    })
+}).catch(error => {
+    console.log(error)
+})
